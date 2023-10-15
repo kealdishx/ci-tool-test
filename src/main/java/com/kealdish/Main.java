a7701760fe0ca9e7f6f90e8f180f7916e23852c2
kealdish <kealdish@gmail.com>
2023-10-15 21:59:29 +0800

    保存 runConfigurations

--- src/main/java/com/kealdish/Main.java
+++ src/main/java/com/kealdish/Main.java
@@ -2,6 +2,7 @@ package com.kealdish;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello world1!");
         System.out.println("Hello world!");
     }
 }
